9f4d58c37054920865ffaca86ac3d59b8cd5d455
danielolasupo02 <danielolasupo02@gmail.com>
2026-02-09 22:29:43 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/unionbankng/monitor/realtime/RealtimeAppMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.unionbankng.monitor.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeAppMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeAppMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unionbankng/monitor/realtime/RealtimeAppMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unionbankng.monitor.realtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeAppMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
